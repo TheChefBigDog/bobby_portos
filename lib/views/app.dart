74894304a085bd1177c94e3b1f9dfb7e321afc8d
BobbyRyanHartono <bobby.hartono@smmf.co.id>
2025-10-19 18:43:23 +0700

    Commit of the day:
     + Add Tab Title to web

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My Portfolio',
+      title: 'Bobby Ryan',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       initialBinding: PortfolioBinding(),
       home: PortfolioView(),
